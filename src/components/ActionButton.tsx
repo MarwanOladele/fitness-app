b731e656564e6257a3cdd18a4f2ec54325010eff
MarwanOladele <oladelemarwan@gmail.com>
2023-05-16 19:39:34 +0100

    Responsive Navbar Implemented

---
+++ src/components/ActionButton.tsx
@@ -0,0 +1,20 @@
+import { SelectedPage } from "@/shared/types";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+type Props = {
+  children: React.ReactNode;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const ActionButton = ({ children, setSelectedPage }: Props) => {
+  return (
+    <AnchorLink
+      className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white cursor-pointer"
+      onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+    >
+      {children}
+    </AnchorLink>
+  );
+};
+
+export default ActionButton;
--- src/pages/navbar/Navbar.tsx
+++ src/pages/navbar/Navbar.tsx
@@ -1,50 +1,103 @@
-import { useState } from "react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hook/mediaQuery";
+import { MdDensityMedium, MdOutlineClose } from "react-icons/md";
+import { useState } from "react";
+import ActionButton from "@/components/ActionButton";
 
 type Props = {
   selectedPage: SelectedPage;
   setSelectedPage: (value: SelectedPage) => void;
 };
 
-const Navbar = ({selectedPage, setSelectedPage}: Props) => {
+const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+  const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
+  const [isNavbarOpen, setIsNavbarOpen] = useState<boolean>(false);
+  console.log(isAboveMediumScreen);
+
   const flexBetween = "flex justify-between items-center";
   return (
     <nav>
+      {/* MOBILE MENU */}
+      {!isAboveMediumScreen && isNavbarOpen && (
+        <div className="fixed right-0 top-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          {/* CLOSE ICON */}
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsNavbarOpen(!isNavbarOpen)}>
+              <MdOutlineClose className="h-10 w-10 text-gray-400" />
+            </button>
+          </div>
+
+          {/* MENU ITEMS */}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Our Classes"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        </div>
+      )}
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
             <img src={Logo} alt="logo" />
-          </div>
-          <div className={`${flexBetween} w-full`}>
-            <div className={`${flexBetween} gap-8 text-sm`}>
-              <Link
-                page="Home"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Benefits"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Our Classes"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Contact Us"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-            </div>
-            <div className={`${flexBetween} gap-8`}>
-              <p>Sign In</p>
-              <button>Become a Member</button>
-            </div>
+
+            {isAboveMediumScreen ? (
+              <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+                <div className={`${flexBetween} gap-8`}>
+                  <p>Sign In</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a Member
+                  </ActionButton>
+                </div>
+              </div>
+            ) : (
+              <button
+                className="rounded-full bg-secondary-500 p-2 "
+                onClick={() => setIsNavbarOpen(!isNavbarOpen)}
+              >
+                <MdDensityMedium className="h-6 w-6 text-white" />
+              </button>
+            )}
           </div>
         </div>
       </div>
