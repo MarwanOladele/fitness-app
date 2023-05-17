5f87a9524b5db33739e51654326bc5c562b435ec
MarwanOladele <oladelemarwan@gmail.com>
2023-05-17 18:35:14 +0100

    Homepage Implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/pages/navbar/Navbar";
 import { useState } from "react";
 import { SelectedPage } from "@/shared/types";
+import Home from "./pages/home/Home";
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div className="app bg-gray-20">
       <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   ); 
 };
