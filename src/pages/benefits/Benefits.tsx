9d7686dedf53d13f7e46d8555d89d1d25b9c8d34
MarwanOladele <oladelemarwan@gmail.com>
2023-06-12 20:46:45 +0100

    benefit page section completed

--- src/pages/benefits/Benefits.tsx
+++ src/pages/benefits/Benefits.tsx
@@ -1,5 +1,4 @@
 import Benefit from "@/components/Benefit";
-import HText from "@/components/HText";
 import { BenefitType, SelectedPage } from "@/shared/types";
 import {
   HomeModernIcon,
@@ -7,6 +6,9 @@ import {
   AcademicCapIcon,
 } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
+import HText from "../../components/HText";
+import ActionButton from "../../components/ActionButton";
+import benefitImg from "../../assets/BenefitsPageGraphic.png";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -59,6 +61,56 @@ const Benefits = ({ setSelectedPage }: Props) => {
               setSelectedPage={setSelectedPage}
             />
           ))}
+        </div>
+
+        {/* GRAPHICS AND DESCRIPTION */}
+
+        <div className="md:flex  items-center justify-between gap-20 mt-16 md:mt-28">
+          {/*  GRAPHICS*/}
+          <img
+            src={benefitImg}
+            alt="benefits-page-graphic"
+            className="mx-auto"
+          />
+
+          {/*  DESCRIPTION*/}
+
+          <div className="">
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-abstractwaves">
+                <div className="">
+                  <HText>
+                    MILLIONS OF HAPPY MEMBER GETTING{"    "}
+                    <span className="text-primary-500">FIT</span>
+                  </HText>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <p className="my-5">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Explicabo fugit minus itaque deleniti aliquam, consequatur
+                perspiciatis voluptatem animi, numquam quis rerum culpa? Dolor,
+                illo animi fuga aut, minima nemo perspiciatis, explicabo
+                recusandae provident accusantium tenetur similique quibusdam
+                esse voluptatem magni et velit a culpa distinctio ratione nulla!
+                Sapiente, unde quo.
+              </p>
+              <p className="mb-5">
+                Explicabo fugit minus itaque deleniti aliquam, consequatur
+                perspiciatis voluptatem animi, numquam quis rerum culpa? Dolor,
+                illo animi fuga aut, minima nemo perspiciatis, explicabo
+                recusandae provident accusantium tenetur similiq
+              </p>
+            </div>
+            <div className="relative mt-16">
+              <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Join Now
+                </ActionButton>
+              </div>
+            </div>
+          </div>
         </div>
       </motion.div>
     </section>
