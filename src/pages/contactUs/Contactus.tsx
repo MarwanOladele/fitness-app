443c899249971bc2c0df0f9683b775a72618740c
MarwanOladele <oladelemarwan@gmail.com>
2023-06-16 12:33:20 +0100

    contact us section

--- src/pages/contactUs/Contactus.tsx
+++ src/pages/contactUs/Contactus.tsx
@@ -1,15 +1,17 @@
-import { SelectedPage } from "@/shared/types"
-import { useForm } from "react-hook-form"
-import contactus from '../../assets/ContactUsPageGraphic.png'
+import { SelectedPage } from "@/shared/types";
+import { useForm } from "react-hook-form";
+import contactus from "../../assets/ContactUsPageGraphic.png";
 
 type Props = {
-    setSelectedPage: (value: SelectedPage) => void
-}
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Contactus = ({setSelectedPage}: Props) => {
+const Contactus = ({ setSelectedPage }: Props) => {
   return (
-    <section id="contactus" className="mx-auto w-5/6">Contactus</section>
-  )
-}
+    <section id="contactus" className="mx-auto w-5/6">
+      Contactus
+    </section>
+  );
+};
 
-export default Contactus
+export default Contactus;
